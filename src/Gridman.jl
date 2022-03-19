d4a22a0035dac4b69866f07b8668bd1c62951e08
YANG Zijiang <yang.zj@live.com>
2022-03-20 03:19:49 +0800

    A toy gauge data structure.

--- src/Gridman.jl
+++ src/Gridman.jl
@@ -1,5 +1,23 @@
 module Gridman
 
+struct Gauge
+  x::Array{ComplexF64}
+  y::Array{ComplexF64}
+  z::Array{ComplexF64}
+  t::Array{ComplexF64}
+  xdim::Int64
+  ydim::Int64
+  zdim::Int64
+  tdim::Int64
+  v::Int64
+
+  function Gauge(xdim, ydim, zdim, tdim)
+    v = xdim * ydim * zdim * tdim;
+    diag = [1 0 0; 0 1 0; 0 0 1];
+    return new(diag, diag, diag, diag, xdim, ydim, zdim, tdim, v);
+  end
+end
+
 greet() = print("Hello World!")
 
 end # module
