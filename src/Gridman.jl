1a17c94276a6858130d521d0d5f8bb9283c088ef
YANG Zijiang <yang.zj@live.com>
2022-03-20 20:17:19 +0800

    Fix bug of Gauge struct member type and propose a toy fermion data structure.

--- src/Gridman.jl
+++ src/Gridman.jl
@@ -1,10 +1,10 @@
 module Gridman
 
 struct Gauge
-  x::Array{ComplexF64}
-  y::Array{ComplexF64}
-  z::Array{ComplexF64}
-  t::Array{ComplexF64}
+  x::Array{Matrix{ComplexF64}}
+  y::Array{Matrix{ComplexF64}}
+  z::Array{Matrix{ComplexF64}}
+  t::Array{Matrix{ComplexF64}}
   xdim::Int64
   ydim::Int64
   zdim::Int64
@@ -14,7 +14,23 @@ struct Gauge
   function Gauge(xdim, ydim, zdim, tdim)
     v = xdim * ydim * zdim * tdim;
     diag = [1 0 0; 0 1 0; 0 0 1];
-    return new(diag, diag, diag, diag, xdim, ydim, zdim, tdim, v);
+    diags = fill(diag, v);
+    return new(diags, diags, diags, diags, xdim, ydim, zdim, tdim, v);
+  end
+end
+
+struct Fermion
+  value::Array{Vector{ComplexF64}}
+  xdim::Int64
+  ydim::Int64
+  zdim::Int64
+  tdim::Int64
+  v::Int64
+
+  function Fermion(xdim, ydim, zdim, tdim)
+    v = xdim * ydim * zdim * tdim;
+    value = [rand(ComplexF64, 12) for v in 1:v];
+    return new(value, xdim, ydim, zdim, tdim, v);
   end
 end
 
