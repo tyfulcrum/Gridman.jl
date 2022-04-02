c37aa5ac7b7e5c56f404257ed12305a0b85ef686
Yang Zijiang <yangzijiang@cnic.cn>
2022-04-02 11:12:18 +0800

    Tensor multiply.

--- src/Gridman.jl
+++ src/Gridman.jl
@@ -1,5 +1,6 @@
 module Gridman
 
+using LinearAlgebra;
 struct Gauge
   x::Vector{Matrix{ComplexF64}}
   y::Vector{Matrix{ComplexF64}}
@@ -34,6 +35,22 @@ struct Fermion
     value = [rand(ComplexF64, 12) for volume in 1:volume];
     return new(value, xdim, ydim, zdim, tdim, volume);
   end
+end
+
+function auto_tensor_mul(U::Matrix{ComplexF64}, χ::Vector{ComplexF64})
+  γ = [0 0 0 im; 0 0 im 0; 0 -im 0 0; -im 0 0 0];
+  ϵ = map(x -> x * U, (I - γ));
+  ϕ = ϵ * reshape(χ, (4, 3));
+  return ϕ;
+end
+
+function hc_tensor_mul(U::Matrix{ComplexF64}, χ::Vector{ComplexF64})
+  ϕ_dirac_1 = U * χ[1:3] + (-im) * U * χ[10:12];
+  ϕ_dirac_4 = im * U * χ[1:3] + U * χ[10:12];
+  ϕ_dirac_2 = U * χ[4:6] + (-im) * U * χ[7:9];
+  ϕ_dirac_3 = im * U * χ[4:6] + U * χ[7:9];
+  ϕ = vcat(ϕ_dirac_1, ϕ_dirac_2, ϕ_dirac_3, ϕ_dirac_4);
+  return ϕ;
 end
 
 greet() = print("Hello World!")
